eabc7e9f9945c0d640edaf878fb7d8ad0029bc79
Jafeth V Villarreal <jafeth.villarreal@globalresale.com>
2021-01-21 08:06:04 -0600

    Create and update a table with migration files

--- aprendiendo-laravel-1/database/migrations/2021_01_21_012304_create_fruits_table.php
+++ aprendiendo-laravel-1/database/migrations/2021_01_21_012304_create_fruits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class CreateFruitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        ///Create a Schema and creats a Users table
+        Schema::create('fruits', function (Blueprint $table) {
             $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->timestamps();
+            $table->string('fruitName');
+            $table->integer('amount');
         });
     }
 
@@ -30,6 +29,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('fruits');
     }
 }
---
+++ aprendiendo-laravel-1/database/migrations/2021_01_21_014019_add_field_fruits.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldFruits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //To ALTER a table
+        Schema::table('fruits', function (Blueprint $table) {
+            $table->string('typeFruit')->after('amount');
+            $table->renameColumn('fruitName','name');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }   
+}
