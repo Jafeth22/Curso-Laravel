bb7337fe911080791852ed2b3f55675d8e96768e
Jafeth VV <jafethvv@hotmail.com>
2021-01-26 17:32:04 -0600

    Create models

--- videos-laravel/app/Video.php
+++ videos-laravel/app/Video.php
@@ -9,8 +9,30 @@ class Video extends Model
     protected $table = 'videos';
 
     //Relation One to Many
+    /**
+     * This will let to the app to relate each comment with its video without relate refresh the page
+     * It means, for this case, relate the video with all its comments
+     */
+    // public function Comments()
+    // {
+    //     return $this->hasMany('App\Comment');//$this->hasMany(Comment::class); //or this other way: 
+    // }
+
+    public function User()
+    {
+        return $this->belongsTo('App\User', 'User_Id');
+    }
+
     public function Comments()
     {
-        # code...
+        return $this->belongsTo('App\Comment', 'ID');
     }
+    
+    public function Commenta2()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    //One to One
+    //$this->hasOne();
 }
--- videos-laravel/routes/web.php
+++ videos-laravel/routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\DB;
+use App\Video;
 
 Route::get('/', function () {
+    $result = Video::all();
+
+    //To know the values of the DB's connection
+    /*if (DB::connection()->getDatabaseName()) {
+        //echo "conncted sucessfully to database " . DB::connection()->getDatabaseName();
+        $databaseName = Config::get('database.connections.' . Config::get('database.default'));
+        dd($databaseName);
+        die();
+    }*/
+    foreach ($result as $data) {
+        echo $data->Title.'---'.$data->User->Name.'<br/>'; //On this line, I'm using the FK to get the name of the user
+        echo $data->Comments->Body;
+        // foreach ($data->Comments as $key => $item) {
+        //     echo '*'.$key.'---'.$item->Body.'<br/>';
+        // }
+        // dd($data->Comments);
+        echo '<hr>';
+    }
+
     return view('welcome');
 });
